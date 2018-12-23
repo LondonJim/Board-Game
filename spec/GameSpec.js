9ef987aeeb415fdb7e08c9d200c7d5ad9e53828c
James Sutherland <jrsutherland78@googlemail.com>
2018-12-23 12:16:54 +0000

    creates game board with jumps

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -5,9 +5,42 @@ describe("Game", () => {
     game = new Game(mockPlayer)
   })
 
-  it('should be move a random number from 1 to 6', () => {
-    expect(game.roll()).toBeGreaterThan(0)
-    expect(game.roll()).toBeLessThan(7)
+  describe("#roll", () => {
+    it('should be move a random number from 1 to 6', () => {
+      expect(game.roll()).toBeGreaterThan(0)
+      expect(game.roll()).toBeLessThan(7)
+    })
+  })
+
+  describe('#createBoard', () => {
+    it('jumping numbers should be between -10 and 10', () => {
+      let isCreateBoardJumps = true
+      gameBoard = game.board
+      for(let row=0; row < gameBoard.length -1; row++) {
+        for(let col=0; col < gameBoard[row].length - 1; col++) {
+          if (gameBoard[row][col] > 10 || gameBoard[row][col] < -10) {
+            isCreateBoardJumps = false
+          }
+        }
+      }
+      expect(isCreateBoardJumps).toEqual(true)
+    })
+
+    it('create a 10x10 game board', () => {
+      let isCreateBoard = true
+      gameBoard = game.board
+      if (gameBoard.length != 10) {
+        isCreateBoard = false
+      } else {
+        for (let row=0; row < 10; row++) {
+          if (gameBoard[row].length != 10) {
+            isCreateBoard = false
+          }
+        }
+      }
+
+      expect(isCreateBoard).toEqual(true)
+    })
   })
 
 })
--- src/Game.js
+++ src/Game.js
@@ -2,9 +2,28 @@ class Game {
 
   constructor(player = new Player) {
     this.player = player
+    this.board = this.createBoard()
   }
 
   roll() {
     return (Math.floor(Math.random()*6)+1)
+  }
+
+  createBoard() {
+    let board = [[],[],[],[],[],[],[],[],[],[]]
+    for (let row=0; row < 10; row++) {
+      for (let col=0; col < 10; col++) {
+        if ((Math.floor(Math.random()*5)) === 1) {
+          board[row].push(this.jumpPlace())
+        } else {
+          board[row].push(0)
+        }
+      }
+    }
+    return board
+  }
+
+  jumpPlace() {
+    return (Math.floor(Math.random()*20) - 10)
   }
 }
