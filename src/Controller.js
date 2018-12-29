9402550a43ab238c3d9d5dc0b765c51a21759061
James Sutherland <jrsutherland78@googlemail.com>
2018-12-29 22:56:54 +0000

    add submit player numbers UI

--- src/Controller.js
+++ src/Controller.js
@@ -1,3 +1,16 @@
 class Controller {
 
+  constructor(game = new Game) {
+    this.game = game
+    this.enterPlayers()
+  }
+
+  enterPlayers() {
+    document.getElementById('submit-players').addEventListener('submit', function(e) {
+      e.preventDefault()
+      let players = document.getElementById('players').value
+      this.game.numberOfPlayers(players)
+      document.getElementById('submit-players').style.display = 'none'
+    }.bind(this))
+  }
 }
