2254d3b87338e0b7aca0aff3805fd75d851df0df
James Sutherland <jrsutherland78@googlemail.com>
2018-12-30 23:14:20 +0000

    add numbered places and movement number to cells

--- src/Controller.js
+++ src/Controller.js
@@ -3,6 +3,7 @@ class Controller {
   constructor(game = new Game) {
     this.game = game
     this.enterPlayers()
+    this.displayBoard()
   }
 
   enterPlayers() {
@@ -12,5 +13,13 @@ class Controller {
       this.game.numberOfPlayers(players)
       document.getElementById('submit-players').style.display = 'none'
     }.bind(this))
+  }
+
+  displayBoard() {
+    for(let place = 0; place < this.game.board.length; place++) {
+      let cell = document.getElementById("cell" + place)
+      cell.style.fontSize = "15px"
+      cell.innerHTML = `${place}/${this.game.board[place]}`
+    }
   }
 }
